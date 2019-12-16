168b9368586b24b3b6506c4b0db990f076e9d6da
sathya <sathya.vakacharla@gmail.com>
2019-12-16 22:50:02 +0530

    functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      Hello Sathya!!
+      <Greet/>
     </div>
   );
 }
---
+++ src/components/Greet.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Greet = () => {
+    return <h1>Hello Sathya!! How are you?</h1>
+}
+
+export default Greet;
