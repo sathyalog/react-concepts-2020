c6124ef050f670a889d1a496c3039420ca3a4067
sathya <sathya.vakacharla@gmail.com>
2019-12-16 22:40:30 +0530

    skeleton ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello Sathya!!
+    </div>
+  );
+}
+
+export default App;
