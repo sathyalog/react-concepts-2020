89a4fb3334bc6836418c634cc960a944a390d184
sathya <sathya.vakacharla@gmail.com>
2019-12-17 15:49:03 +0530

    class component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
       <Greet/>
+      <Greeting />
     </div>
   );
 }
---
+++ src/components/Greeting.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Greeting extends Component {
+    render() {
+        return <h2> This is class component</h2>
+    }
+}
+
+export default Greeting;
